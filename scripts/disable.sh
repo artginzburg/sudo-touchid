ea19c19d21bb70849743b39c5b5b908299a99655
artginzburg <art.ginzburg@ya.ru>
2021-09-15 12:30:13 +0300

    rename uninstall.sh to disable.sh

--- scripts/disable.sh
+++ scripts/disable.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-sudo_touchid_uninstall() {
+sudo_touchid_disable() {
   local touch_pam='auth       sufficient     pam_tid.so'
   local sudo_path='/etc/pam.d/sudo'
 
   if grep -e "^$touch_pam$" "$sudo_path" &> /dev/null; then
-    echo "The following will be your $sudo_path after uninstallation:\n"
+    echo "The following will be your $sudo_path after disabling:\n"
     grep -v "^$touch_pam$" "$sudo_path"
     echo
     read -p "Are you sure? [y] to confirm " -n 1 -r
@@ -17,4 +17,4 @@ sudo_touchid_uninstall() {
     echo "TouchID for sudo seems not to be enabled"
   fi
 }
-sudo_touchid_uninstall
+sudo_touchid_disable
